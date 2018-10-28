c2c351ef44d6906d8fdafd0d81d0101351b7a94b
Zakeeruddin Mohammed <zmoha10@safeway.com>
2018-10-28 15:36:47 -0700

    Adding a shared function

---
+++ vars/appArtifact.groovy
@@ -0,0 +1,24 @@
+def getNewArtifactVersion(build, majorVersion, minorVersion) {
+  int patch=getLastArtifactPatchVersion(build.getPreviousBuild());
+  patch+=1;
+  String version = majorVersion+"."+minorVersion+"."+patch.toString();
+  // setBuildNameAndDescription(version, "SCM Checkout");
+  return version;
+}
+
+
+def int getLastArtifactPatchVersion(build) {
+	if (build == null) {
+		return 0;
+	}
+
+	if(build != null
+			&& build.buildVariables.appEnvironment.toString().equals("dev")
+			&& (build.buildVariables.containsKey("artifactVersion")
+				&& !build.buildVariables.artifactVersion.toString().equals("")))
+	{
+		return build.buildVariables.artifactVersion.toString().split("\\.")[2].toInteger();
+	}
+	//Recurse now to handle in chronological order
+	getLastArtifactPatchVersion(build.getPreviousBuild());
+}
