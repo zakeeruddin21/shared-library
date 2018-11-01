0be290c51ed5b8b30a256d435e8990fe0d8883ed
Zakeeruddin Mohammed <zmoha10@safeway.com>
2018-10-31 17:43:09 -0700

    commit 7

--- resources/test.sh
+++ resources/test.sh
@@ -1,7 +1,7 @@
 #source test1.sh
 export TEMP='test.sh'
-echo "${TEST1SCRIPT}"
-echo "${TEST2SCRIPT}"
-echo "${TEST3SCRIPT}"
+eval "${TEST1SCRIPT}"
+eval "${TEST2SCRIPT}"
+eval "${TEST3SCRIPT}"
 echo "From test.sh. The value of TEMP is ${TEMP}"
 echo "${FILE1} ${FILE2} - ${NAME1}"
